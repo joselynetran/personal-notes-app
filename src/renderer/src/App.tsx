c3e941e9ce291630a19f06c82c1b8c6db773ff93
joselynetran <joselyne.tran@gmail.com>
2024-11-30 21:40:06 -0500

    intializinggg

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,9 @@
-import electronLogo from './assets/electron.svg'
-
-function App(): JSX.Element {
-  return <div></div>
+function App() {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <span className="text-4xl text-blue-500">Hello from Electron</span>
+    </div>
+  )
 }
 
 export default App
